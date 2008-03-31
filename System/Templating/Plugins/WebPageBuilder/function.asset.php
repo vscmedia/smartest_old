8662f7076e5dd602ae9ffa7258054da25a70ea5e
Marcus Gilroy-Ware <marcus@vsccreative.com>
2008-03-31 21:24:03 +0000

    <?sm:asset:?> tag updated to use renderAssetById($id, $params, $path)
    git-svn-id: svn://svn.vsclabs.com/smartest@73 3b18a885-9e44-0410-b831-aac8df1b01cb

--- System/Templating/Plugins/WebPageBuilder/function.asset.php
+++ System/Templating/Plugins/WebPageBuilder/function.asset.php
@@ -2,6 +2,18 @@
 
 function smarty_function_asset($params, &$smarty){
     
-    return $smarty->renderAsset($params);
+    if(isset($params['id']) && is_numeric($params['id'])){
+        $asset_id = $params['id'];
+    }else if(isset($params['name']) && strlen($params['name'])){
+        $asset_id = $params['name'];
+    }
+    
+    if(isset($params['path'])){
+        $path = (!in_array($params['path'], array('file', 'full'))) ? 'none' : $params['path'];
+    }else{
+        $path = 'none';
+    }
+    
+    return $smarty->renderAssetById($asset_id, $params, $path);
     
 }
