004f08ca77672f61ac5ac7d798b1ea7e637a8a87
Marcus Gilroy-Ware <marcus@vsccreative.com>
2009-07-08 11:42:37 +0000

    Small change to the installer
    git-svn-id: svn://svn.vsclabs.com/smartest@246 3b18a885-9e44-0410-b831-aac8df1b01cb

--- System/Install/Screens/set_perms.php
+++ System/Install/Screens/set_perms.php
@@ -17,15 +17,28 @@ foreach($errors as $file){
 </ul>
 
 <?php if ($stage->g('perms')->g('script_created')): ?>
-<p>Smartest has helpfully created a bash script to do all this for you. To run it, type the following as root.</p>
-<p><code>bash <?php echo $stage->g('perms')->g('script_name'); ?></code></p>
+  <?php $u = posix_getpwuid(fileowner(SM_ROOT_DIR.$errors[0].'.')); ?>
+<p>Smartest has helpfully created a bash script to do all this for you.</p>
+
+<p>To check the script before you run it, type:
+  <code>
+    less <?php echo $stage->g('perms')->g('script_name'); ?>
+  </code>
+</p>
+
+<p>To run it, type the following (preferably as user <strong><?php echo $u['name']?></strong> or <strong>root</strong>).
+  <code>
+    bash <?php echo $stage->g('perms')->g('script_name'); ?>
+  </code>
+</p>
+
+<p>Alternatively if you're not comfortable using a terminal, get a friend or your server administrator to do it.</p>
+<p>Once you've done this, click "Next"</p>
 <?php else: ?>
 <p>To make a directory writable, log into your server with a terminal and type this:</p>
 <p><code>chmod 777 <?php echo $errors[0]; ?></code></p>
-<?php endif; ?>
-
 <p>Alternatively if you're not comfortable using a terminal, get a friend or your server administrator to do it.</p>
-
 <p>Once you've done this for each of the folders listed above, click "Next"</p>
+<?php endif; ?>
 
 <div class="button normal-button"><a href="<?php echo $_SERVER['REQUEST_URI']; ?>">Next</a></div>
