039e339a11d8f26a8aa13ae80db543a574823d5f
Marcus Gilroy-Ware <marcus@vsccreative.com>
2012-08-08 17:11:42 +0000

    tweak to mass-publish tool
    git-svn-id: svn://svn.vsclabs.com/smartest@515 3b18a885-9e44-0410-b831-aac8df1b01cb

--- System/Data/BasicObjects/SmartestModel.class.php
+++ System/Data/BasicObjects/SmartestModel.class.php
@@ -472,7 +472,7 @@ class SmartestModel extends SmartestBaseModel{
     
     public function getPublishableSimpleItems($site_id, $user_id='', $include_unapproved_items=false){
         
-        $sql = "SELECT DISTINCT Items.* FROM Items, ItemPropertyValues WHERE (Items.item_public='FALSE' OR (ItemPropertyValues.itempropertyvalue_content != ItemPropertyValues.itempropertyvalue_draft_content)) AND ItemPropertyValues.itempropertyvalue_item_id = Items.item_id AND Items.item_deleted='0' AND Items.item_site_id='".$site_id."' AND Items.item_itemclass_id='".$this->getId()."'";
+        $sql = "SELECT Items.* FROM Items, ItemPropertyValues WHERE (Items.item_public='FALSE' OR (ItemPropertyValues.itempropertyvalue_content != ItemPropertyValues.itempropertyvalue_draft_content)) AND ItemPropertyValues.itempropertyvalue_item_id = Items.item_id AND Items.item_deleted='0' AND Items.item_site_id='".$site_id."' AND Items.item_itemclass_id='".$this->getId()."'";
         
         if(!$include_unapproved_items){
             $sql .= 'AND Items.item_changes_approved=\'1\'';
@@ -482,11 +482,15 @@ class SmartestModel extends SmartestBaseModel{
         
         $results = $this->database->queryToArray($sql);
         $items = array();
+        $ids = array();
         
         foreach($results as $r){
-            $item = new SmartestItem;
-            $item->hydrate($r);
-            $items[] = $item;
+            if(!isset($ids[$r['item_id']])){
+                $item = new SmartestItem;
+                $item->hydrate($r);
+                $items[] = $item;
+                $ids[$r['item_id']] = 1;
+            }
         }
         
         return $items;
