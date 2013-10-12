f56e12eb0372996110b3cfa1bfcd557971ab1d3e
Marcus Gilroy-Ware <marcus@vsccreative.com>
2013-10-12 10:16:04 +0000

    Fixed a bug where a template used a PHP function that was not allowed; Fixed a bug where the installer did not grant enough permissions to initial user. Initial user is now root and also has permission to grant themselves permissions in case they lose root permission
    git-svn-id: svn://svn.vsclabs.com/smartest@606 3b18a885-9e44-0410-b831-aac8df1b01cb

--- System/Templating/SmartestEngine.class.php
+++ System/Templating/SmartestEngine.class.php
@@ -50,7 +50,7 @@ class SmartestEngine extends Smarty{
 		$this->security_settings['PHP_HANDLING'] = false;
 		$this->security_settings['PHP_TAGS'] = false;
 		$this->security_settings['MODIFIER_FUNCS'] = array('strtolower', 'strtoupper', 'trim', 'addslashes', 'stripslashes');
-		$this->security_settings['IF_FUNCS'] = array('strlen', 'empty', 'count', 'in_array', 'array', 'is_numeric', 'is_array');
+		$this->security_settings['IF_FUNCS'] = array('strlen', 'empty', 'count', 'in_array', 'array', 'is_numeric', 'is_array', 'isset');
 		$this->security_settings['INCLUDE_ANY'] = true;
 		
 	}
